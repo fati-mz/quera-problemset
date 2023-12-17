743f19ef395c89b0c3143917052fbf300ed03bd2
Raya <84349659+raya-mansouri@users.noreply.github.com>
2023-12-17 20:25:44 +0330

    Create 14580.py

---
+++ 14580.py
@@ -0,0 +1,10 @@
+n, x, k = map(int, input().split())
+networks = [input() for _ in range(n)]
+
+current_network = x - 1
+for _ in range(k):
+    current_network = (current_network + 1) % n
+
+final_network = networks[current_network]
+
+print(final_network)
