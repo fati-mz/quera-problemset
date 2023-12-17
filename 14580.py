3f650ee2dc4e741cda06c0461767b730ffc39c88
Raya <84349659+raya-mansouri@users.noreply.github.com>
2023-12-17 20:38:48 +0330

    1402-9-26

--- 14580.py
+++ 14580.py
@@ -1,10 +1,10 @@
 n, x, k = map(int, input().split())
 networks = [input() for _ in range(n)]
 
-current_network = x - 1
-for _ in range(k):
-    current_network = (current_network + 1) % n
+#current_network = x - 1
+#for _ in range(k):
+#    current_network = (current_network + 1) % n
+#final_network = networks[current_network]
+#print(final_network)
 
-final_network = networks[current_network]
-
-print(final_network)
+print(networks[(x+(k%n))%n-1])
