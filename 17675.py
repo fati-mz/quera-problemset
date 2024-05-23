ea0cd1f243b0c2e5aeef8afc6a84385009e92178
Raya <84349659+raya-mansouri@users.noreply.github.com>
2024-05-23 16:08:48 +0330

    14

---
+++ 17675.py
@@ -0,0 +1,20 @@
+import math
+
+def is_perfect_square(x):
+    s = int(math.sqrt(x))
+    return s * s == x
+
+def is_fibonacci(n):
+    # 5*n*+4 or 5*n*-4 are perfect square
+    return is_perfect_square(5 * n * n + 4) or is_perfect_square(5 * n * n - 4)
+
+num = int(input())
+
+s=''
+for i in range(1,num+1):
+    if is_fibonacci(i):
+        s+='+'
+    else:
+        s+='-'
+
+print(s)
